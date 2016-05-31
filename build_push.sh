e122761d7ffc9ef4e2c192eedcd516b7b939ab4e
Lab Account <lab@atomcool.rice.edu>
2016-05-31 12:16:04 -0500

    modify the publish script

--- build_push.sh
+++ build_push.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-bundle exec jekyll build --config _config_atomcool.yml, _config.yml
+#bundle exec jekyll build --config _config_atomcool.yml, _config.yml
+bundle exec jekyll build
 chown lab:www-data ./_site -R
-cp -r ./_site/* /var/www
+cp -rf ./_site/* /var/www
 
